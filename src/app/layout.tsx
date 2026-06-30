8bd707678aa24e3b5bff7737da38542660cee7ef
SquellyFineOfficiel <100866938+SquellyFineOfficiel@users.noreply.github.com>
2026-06-30 12:04:41 +0200

    Analytics fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {`(function(w,d,s,l,i) {w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TNQ838BF');`}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-41ZERN3XJE"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-41ZERN3XJE');
+          `}
+        </Script>
+        {/* End Google tag (gtag.js) */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
